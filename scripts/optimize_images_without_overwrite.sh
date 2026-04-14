c0036349fcc6e4517483fb59775696d67aa7c157
arekpaterak <arkadiusz.paterak@gmail.com>
2026-04-14 19:24:23 +0200

    Add more media partners

--- scripts/optimize_images_without_overwrite.sh
+++ scripts/optimize_images_without_overwrite.sh
@@ -57,6 +57,7 @@ optimize_images institutions-logos 600x600 webp 90
 optimize_images speakers 600x600 webp 90
 
 optimize_images sponsors 600x600 webp 90
+optimize_images partners 600x600 webp 90
 
 optimize_images ai-generated 800x800 webp 90
 
